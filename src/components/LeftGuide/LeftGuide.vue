c66d9d04af5a1766f48e31304c3f7defd41fb4bf
jinxiao.zhang <17817184057@163.com>
2019-04-25 11:08:09 +0800

    feat(*):完成侧边栏滚动

--- src/components/LeftGuide/LeftGuide.vue
+++ src/components/LeftGuide/LeftGuide.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="left-guide">
         <div class="left-menu">
-            <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :collapse="leftMenu">
+            <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :collapse="leftMenu"
+                     background-color="#545c64"
+                     text-color="#fff"
+                     active-text-color="#ffd04b">
                 <el-submenu index="1">
                     <template slot="title">
                         <i class="el-icon-location"></i>
@@ -30,41 +33,23 @@
                     <i class="el-icon-setting"></i>
                     <span slot="title">导航四</span>
                 </el-menu-item>
-                <el-menu-item index="2">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <i class="el-icon-document"></i>
-                    <span slot="title">导航三</span>
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">导航四</span>
                 </el-menu-item>
                 <el-menu-item index="4">
                     <i class="el-icon-setting"></i>
                     <span slot="title">导航四</span>
                 </el-menu-item>
-                <el-menu-item index="2">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <i class="el-icon-document"></i>
-                    <span slot="title">导航三</span>
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">导航四</span>
                 </el-menu-item>
                 <el-menu-item index="4">
                     <i class="el-icon-setting"></i>
                     <span slot="title">导航四</span>
                 </el-menu-item>
-                <el-menu-item index="3">
-                    <i class="el-icon-document"></i>
-                    <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                    <i class="el-icon-setting"></i>
-                    <span slot="title">导航四</span>
-                </el-menu-item>
-
             </el-menu>
-
         </div>
     </div>
 </template>
@@ -74,7 +59,93 @@
         name: "left-guide",
         data() {
             return {
-                isCollapse: false
+                isCollapse: false,
+                navs: [
+                    {
+                        icon: 'el-icon-lx-home',
+                        index: 'dashboard',
+                        title: '首页'
+                    },
+                    {
+                        icon: 'el-icon-lx-cascades',
+                        index: 'table',
+                        title: '基础表格'
+                    },
+                    {
+                        icon: 'el-icon-lx-copy',
+                        index: 'tabs',
+                        title: 'tab选项卡'
+                    },
+                    {
+                        icon: 'el-icon-lx-calendar',
+                        index: '3',
+                        title: '表单相关',
+                        subs: [
+                            {
+                                index: 'form',
+                                title: '基本表单'
+                            },
+                            {
+                                index: '3-2',
+                                title: '三级菜单',
+                                subs: [
+                                    {
+                                        index: 'editor',
+                                        title: '富文本编辑器'
+                                    },
+                                    {
+                                        index: 'markdown',
+                                        title: 'markdown编辑器'
+                                    },
+                                ]
+                            },
+                            {
+                                index: 'upload',
+                                title: '文件上传'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-lx-emoji',
+                        index: 'icon',
+                        title: '自定义图标'
+                    },
+                    {
+                        icon: 'el-icon-lx-favor',
+                        index: 'charts',
+                        title: 'schart图表'
+                    },
+                    {
+                        icon: 'el-icon-rank',
+                        index: '6',
+                        title: '拖拽组件',
+                        subs: [
+                            {
+                                index: 'drag',
+                                title: '拖拽列表',
+                            },
+                            {
+                                index: 'dialog',
+                                title: '拖拽弹框',
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-lx-warn',
+                        index: '7',
+                        title: '错误处理',
+                        subs: [
+                            {
+                                index: 'permission',
+                                title: '权限测试'
+                            },
+                            {
+                                index: '404',
+                                title: '404页面'
+                            }
+                        ]
+                    }
+                ]
             };
         }
         , computed: {
@@ -98,11 +169,17 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
     .left-menu
-        width 200px
-        min-height 400px
-        overflow-y:hidden
-        max-height 100%
-
+        /*width 200px*/
+        min-height 100%
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 50px;
+        bottom:0;
+        overflow-y: scroll;
+    .left-menu::-webkit-scrollbar {
+        display:none
+    }
     .el-menu-vertical-demo:not(.el-menu--collapse) {
         width: 200px;
         min-height: 400px;
@@ -114,5 +191,9 @@
         height:auto !important
     .el-menu-item-group>ul{
         height:auto !important
+    }
+
+    .left-menu> ul {
+        height:100%;
     }
 </style>
